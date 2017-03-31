ae7e9fc99e4b974537836abef41e73ac2dfaf244
arokem <arokem@gmail.com>
2017-03-31 16:25:24 -0700

    PEP8 in the setup file.

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup
 setup(
-    name = 'data-pack',
-    packages = ['datapack'],
-    version = '0.0',
-    description = 'Data packages',
-    author = 'Stéfan van der Walt',
-    author_email = 'stefanv@berkeley.edu',
-    url = 'https://github.com/data-pack/data-pack',
-    download_url = 'https://github.com/data-pack/data-pack/archive/0.0.tar.gz',
-    keywords = ['data', 'packaging', 'science'],
-    classifiers = [],
+    name='data-pack',
+    packages=['datapack'],
+    version='0.0',
+    description='Data packages',
+    author='Stéfan van der Walt',
+    author_email='stefanv@berkeley.edu',
+    url='https://github.com/data-pack/data-pack',
+    download_url='https://github.com/data-pack/data-pack/archive/0.0.tar.gz',
+    keywords=['data', 'packaging', 'science'],
+    classifiers=[],
     requires=["requests"]
 )
