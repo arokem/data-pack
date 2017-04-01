57bfdadc06ed45fe38e5ee8c4313b9678af9e617
arokem <arokem@gmail.com>
2017-03-31 17:49:22 -0700

    Maybe I need to change the name of the thingee?

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='data-pack',
+    name='datapack',
     packages=find_packages(),
     version='0.0',
     description='Data packages',
